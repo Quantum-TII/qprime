fc02b99e016fabc0f0faef8ff2236d274f60b25b
Stefano Carrazza <stefano.carrazza@cern.ch>
2020-01-25 20:20:52 +0100

    renaming script

--- examples/primes_mpf.py
+++ examples/primes_mpf.py
@@ -6,8 +6,8 @@ import argparse
 from qprime.api import entropy_mpf
 
 
-def main(n):
-    e = entropy_mpf(n)
+def main(n, bits):
+    e = entropy_mpf(n, bits=bits)
     print('\n> Final entropy', e)
     with open('entropy_n%d.dat' % n, 'w') as f:
         f.write('%d %s\n' % (n, e))
@@ -16,5 +16,6 @@ def main(n):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Prime numbers entropy')
     parser.add_argument('n', type=int, help='an integer for the number of qbits')
+    parser.add_argument('--bits', type=int, default=113, help='number of bits for the operation, default 113 (quadruple precision)')
     args = parser.parse_args()
-    main(args.n)
+    main(args.n, args.bits)
